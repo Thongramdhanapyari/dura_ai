4b2fb7738c39964736868b521b2154a944a88000
Thongramdhanapyari <thongramdhanapyari@gmail.com>
2026-03-09 19:04:25 +0530

    added some function to dura namely get_wikipedia(),get_system_stats(),get_weather(),get_news() and changed the loop according to it

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import speech_recognition as sr
 import ollama
 import time
 import webbrowser
+import wikipedia
+import psutil
+import requests
+import feedparser
 
 def speak(text):
     print(f"Dura:{text}")
@@ -11,6 +15,45 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     time.sleep(0.2)
+    
+# wikipedia files
+def get_wikipedia(query):
+    try:
+        result = wikipedia.summary(query,sentences=2)
+        return result
+    except Exception:
+        return "I could not find a specfic entry for that,but I can search Google for you."
+    
+#system stats
+def get_system_stats():
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+    return f"CPU is at {cpu} percent and memory usage is at {ram} percent."
+    
+#weather
+def get_weather():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=28.61&longitude=77.20&current_weather=true"
+    try:
+        response = requests.get(url).json()
+        temp = response['current_weather']['temperature']
+        return f"The current temperature is {temp} degrees celsius."
+    except Exception as e:
+        return "I couldn't reach the weather satellite."
+    
+
+def get_news():
+    # Using Google News RSS feed for "India"
+    rss_url = "https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en"
+    try:
+        feed = feedparser.parse(rss_url)
+        headlines = []
+        # Get the top 3 headlines
+        for entry in feed.entries[:3]:
+            headlines.append(entry.title)
+        
+        return "Here are the top stories. " + " ... ".join(headlines)
+    except Exception:
+        return "I'm having trouble reading the news feeds right now."
     
 def agent_llama(user_input):
     try:
@@ -49,16 +92,32 @@ if __name__=="__main__":
             word= r.recognize_google(audio,language='en-in').lower()
             print(f"you say {word}")
             
+            if "goodbye dura" in word or "stop" in word:
+                speak("Shutting down. Goodbye!")
+                break
             if "dura" in word.lower():
                 user_input = word.replace("dura", "").strip()
-                if user_input:
-                    reply = agent_llama(user_input)
-                else:
-                    reply=" Yes? "
+                if not user_input:
+                    speak("yes?")
                     
-            if "goodbye dura" in word or "stop" in word:
-                        speak("Shutting down. Goodbye!")
-                        break
+                elif "who is" in user_input or "what is" in user_input:
+                    topic = user_input.replace("who is", "").replace("what is", "").strip()
+                    speak(get_wikipedia(topic))
+                    
+                elif "system status" in user_input or "performance" in user_input:
+                    speak(get_system_stats())
+                    
+                elif "news" in word or "headlines" in word:
+                    headlines = get_news()
+                    speak(headlines)
+                    
+                elif "weather" in word:
+                    report = get_weather()
+                    speak(report)
+                    
+                else:
+                    reply=agent_llama(user_input)
+                    speak(reply)
         except sr.UnknownValueError:
             continue 
         except Exception as e:
